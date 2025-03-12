7ea33a2a2667636f72d802bd0bbe1b38395fafd8
Keyyy <rizkyabr698@gmail.com>
2025-03-12 14:00:13 +0700

    1st

---
+++ app/src/main/java/com/android_222102510r/edittext/MainActivity.java
@@ -0,0 +1,41 @@
+package com.android_222102510r.edittext;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button okButton = (Button)findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText namaEditText = (EditText)findViewById(R.id.namaEditText);
+                String nama = namaEditText.getText().toString();
+                String pesan = "Hey, " + nama + "!, " + "How are you?";
+                TextView infoTextView = (TextView)findViewById(R.id.infoTextView);
+                infoTextView.setText(pesan);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
